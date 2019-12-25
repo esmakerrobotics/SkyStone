14aaabfe2a27adf473e7fb75aad8bdbb853daa4a
Spencer <36749347+Spencer981120@users.noreply.github.com>
2019-12-25 20:40:27 +0800

    servo.setPosition 微调到+/- 0.005

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sbKelly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sbKelly.java
@@ -38,26 +38,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- * <p>
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- * <p>
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @TeleOp(name = "Basic: Linear OpMode")
 //@Disabled
+//Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
 public class sbKelly extends LinearOpMode {
 
-    // Declare OpMode members.
-//    private ElapsedTime runtime = new ElapsedTime();
+    //Declare OpMode members.
+    //private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFwd = null;
     private DcMotor leftAft = null;
     private DcMotor rightFwd = null;
@@ -89,9 +76,9 @@ public class sbKelly extends LinearOpMode {
 
             //Servo control start
             if (gamepad1.left_bumper) {
-                servo.setPosition(servo.getPosition() + 0.001);
+                servo.setPosition(servo.getPosition() + 0.005);
             } else if (gamepad1.right_bumper) {
-                servo.setPosition(servo.getPosition() - 0.001);
+                servo.setPosition(servo.getPosition() - 0.005);
             }
 
         }
